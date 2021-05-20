55d28755dfcca2bc0b34d14085a08d55c51ed31c
Koichi.Shimomura <unieye51@gmail.com>
2021-05-20 14:29:08 +0900

    5-42: Routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+import Layout from '@/views/Layout.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
+  {path: '/login', component: Login},
+  {path: '/register', component: Register},  
+  {path: '', component: Layout}
 ]
 
 const router = new VueRouter({
