53e8cdd5800f133ada35465ef397d804b706f693
Koichi.Shimomura <unieye51@gmail.com>
2021-05-20 14:51:30 +0900

    5-43: Regsiter

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,25 +1,65 @@
 <template>
   <main class="form-signin">
-    <form>
-      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <form @submit.prevent="submit">
+      <h1 class="h3 mb-3 fw-normal">Please register</h1>
 
       <div class="form-floating">
-        <input type="email" class="form-control" placeholder="name@example.com">
+        <input v-model="first_name" class="form-control" placeholder="First Name">
+        <label>First Name</label>
+      </div>
+
+      <div class="form-floating">
+        <input v-model="last_name" class="form-control" placeholder="Last Name">
+        <label>Last Name</label>
+      </div>
+
+      <div class="form-floating">
+        <input v-model="email" type="email" class="form-control" placeholder="name@example.com">
         <label>Email address</label>
       </div>
+
       <div class="form-floating">
-        <input type="password" class="form-control" placeholder="Password">
+        <input v-model="password" type="password" class="form-control" placeholder="Password">
         <label>Password</label>
       </div>
 
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <div class="form-floating">
+        <input v-model="password_confirm" type="password" class="form-control" placeholder="Password  Confirm">
+        <label>Password Confirm</label>
+      </div>      
+
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
     </form>
   </main>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Register",
+  data(){
+    return {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      password_confirm: '',
+    }
+  },
+  methods: {
+    async submit() {
+      await axios.post('http://localhost:8000/api/admin/register', {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        password: this.password,
+        password_confirm: this.password_confirm,
+      });
+
+      await this.$router.push('/login');
+    }
+  }
 }
 </script>
 
