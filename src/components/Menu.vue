e5f76f2a3c5ec0dd686ba9a514af0cc6e9e64c8a
Koichi.Shimomura <unieye51@gmail.com>
2021-05-31 11:26:31 +0900

    5-54: Orders

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -12,6 +12,11 @@
             Products
           </router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/orders" class="nav-link" active-class="active">  
+            Orders
+          </router-link>
+        </li>
       </ul>
     </div>
   </nav>
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ import Users from '@/views/Users.vue'
 import Links from '@/views/Links.vue'
 import Products from '@/views/products/Products.vue'
 import ProductForm from '@/views/products/ProductForm.vue'
+import Orders from '@/views/Orders.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,7 @@ const routes: Array<RouteConfig> = [
       {path: '/products', component: Products},
       {path: '/products/create', component: ProductForm},
       {path: '/products/:id/edit', component: ProductForm},
+      {path: '/orders', component: Orders},
     ]
   }
 ]
